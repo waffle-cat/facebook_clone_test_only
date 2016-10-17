e36a9a2fc009ad3bd175eb4df4f1c5304d4d5cf8
waffle-cat <xyng735@gmail.com>
2016-10-17 17:26:36 +0800

    Passed all rspec test for user model with bcrypt and after removing password and password_confirmation column.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,20 @@
 class User < ActiveRecord::Base
 	has_many :statuses, dependent: :destroy
 	has_many :likes, dependent: :destroy
+	has_secure_password
 
 	validates :name, uniqueness: true, presence: true
 	
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	validates :email, uniqueness: true, presence: true
 	
-	validates :password, 
-		presence: { on: :create }, 
+	# validates :password, 
+	# 	presence: { on: :create }, 
+	# 	length: { minimum: 6, maximum: 20 },
+	# 	confirmation: true
+	validates :password,  
 		length: { minimum: 6, maximum: 20 },
 		confirmation: true
-	validates :password_confirmation, presence: true
-	
+	validates :password_confirmation, presence: true, on: :create
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,9 @@ ActiveRecord::Schema.define(version: 20161017042801) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.string   "password_confirmation"
     t.string   "password_digest"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
