f71953f38364b8507ca254d1cdeb106554a016dd
waffle-cat <xyng735@gmail.com>
2016-10-18 17:33:43 +0800

    Passed all rspec tests for statuses controller.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -5,15 +5,17 @@ class StatusesController < ApplicationController
 	end
 
 	def new
-
+		@status = Status.new
 	end
 
 	def create
-		status = current_user.Status.new(status_params)
+		status = current_user.statuses.new(status_params)
 
 		if status.save
-
+			flash[:notice] = "Status is created successfully."
+			redirect_to "/statuses/#{status.id}"
 		else
+			flash[:alert] = "Error creating status."
 			render :new
 		end
 	end
@@ -23,18 +25,32 @@ class StatusesController < ApplicationController
 	end
 
 	def edit
-
+		@status = Status.find(params[:id])
 	end
 
 	def update
+		@status = Status.find(params[:id])
+
+		if @status.update(status_params)
+			flash[:notice] = "Status is updated successfully."
+			redirect_to status_path
+		else
+			render :edit
+		end 
 	end
 
 	def destroy
+
+		status = Status.find(params[:id])
+		status.destroy
+
+		flash[:notice] = "Status is deleted."
+		redirect_to statuses_path
 	end
 
 	private
 		def status_params
-			params.require(:user).permit(:title, :content)
+			params.require(:status).permit(:title, :content)
 		end
 
 end
