27f9cdcbefb977b53256d023c4957769835c09e1
waffle-cat <xyng735@gmail.com>
2016-10-19 14:38:57 +0800

    Refactored validations for user model to be more readable.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
 	has_many :likes, dependent: :destroy
 	has_secure_password
 
-	validates :name, uniqueness: true, presence: true
+	validates_presence_of :name
+	validates_uniqueness_of :name
 	
+
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	validates :email, uniqueness: true, presence: true
 	
@@ -12,9 +14,11 @@ class User < ActiveRecord::Base
 	# 	presence: { on: :create }, 
 	# 	length: { minimum: 6, maximum: 20 },
 	# 	confirmation: true
-	validates :password,  
-		length: { minimum: 6, maximum: 20 },
-		confirmation: true
-	validates :password_confirmation, presence: true, on: :create
+
+	validates_length_of :password, minimum: 6
+	validates_length_of :password, maximum: 20
+
+	validates_confirmation_of :password
+	validates_presence_of :password_confirmation, on: :create
 
 end
